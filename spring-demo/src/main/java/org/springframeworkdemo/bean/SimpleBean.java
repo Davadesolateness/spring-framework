12981cba4ea79798af7aa6f53b3665ff1e0621c0
董知博 <71405284>
2023-09-22 17:08:21 +0800

    增加spring测试配置类

---
+++ spring-demo/src/main/java/org/springframeworkdemo/bean/SimpleBean.java
@@ -0,0 +1,15 @@
+package org.springframeworkdemo.bean;
+
+import org.springframework.context.annotation.Bean;
+
+import java.beans.JavaBean;
+
+/**
+ * @description: TODO
+ * @author dongzhibo
+ * @date 2023/9/20 16:32
+ * @version 1.0
+ */
+@Bean
+public class SimpleBean {
+}
---
+++ spring-demo/src/main/java/org/springframeworkdemo/config/MyConfig.java
@@ -0,0 +1,13 @@
+package org.springframeworkdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: TODO
+ * @author dongzhibo
+ * @date 2023/9/20 16:59
+ * @version 1.0
+ */
+@Configuration
+public class MyConfig {
+}
